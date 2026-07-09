e983dcec0c5560ca65c6758470b29cfca6905438
ha-huy-tan <huytanha2010@gmail.com>
2026-07-09 21:41:30 +0700

    Added minigame
    Playable character, and moving background.

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -1,9 +1,10 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -500.0
 
+# Reference your sprite node here (change "Sprite2D" if named differently)
+@onready var sprite: Sprite2D = $Sprite2D
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -11,14 +12,19 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("ui_up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
+		
+		# Flip the sprite based on direction
+		if direction < 0:
+			sprite.flip_h = true   # Face left
+		elif direction > 0:
+			sprite.flip_h = false  # Face right
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
